cece091e507f94552b26cfdb312711ad408cb4a9
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-06-25 15:52:35 +0300

    Задача-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -3,7 +3,28 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-const flatpickrID = document.querySelector('input#datetime-picker');
+const timerEl = document.querySelector('.timer');
+const daysEl = timerEl.querySelector('[data-days]');
+const hoursEl = timerEl.querySelector('[data-hours]');
+const minutesEl = timerEl.querySelector('[data-minutes]');
+const secondsEl = timerEl.querySelector('[data-seconds]');
+const fieldValueEl = document.querySelectorAll('.value');
+const startButton = document.querySelector('[data-start]');
+let selectedDate = null;
+startButton.disabled = true;
+let countdownInterval;
+timerEl.setAttribute(
+  'style',
+  'font-size: 20px; text-transform: uppercase; display: flex; gap: 20px'
+);
+fieldValueEl.forEach(
+  el => (
+    (el.style.fontSize = '50px'),
+    (el.style.display = 'flex'),
+    (el.style.flexDirection = 'column'),
+    (el.style.alignItems = 'center')
+  )
+);
 
 const options = {
   enableTime: true,
@@ -12,9 +33,15 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+    selectedDate = selectedDates[0];
+    if (selectedDate < new Date()) {
+      Notiflix.Notify.warning('Please choose a date in the future');
+    } else {
+      startButton.disabled = false;
+    }
   },
 };
-flatpickr('#datetime-picker', options);
+
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
@@ -33,3 +60,27 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+function startCountdown() {
+  countdownInterval = setInterval(() => {
+    const currentTime = new Date().getTime();
+    const timeDifference = selectedDate - currentTime;
+    if (timeDifference <= 0) {
+      clearInterval(countdownInterval);
+      Notiflix.Report.success('The countdown has reached zero!');
+      return;
+    }
+
+    startButton.disabled = true;
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+
+    daysEl.textContent = addLeadingZero(days);
+    hoursEl.textContent = addLeadingZero(hours);
+    minutesEl.textContent = addLeadingZero(minutes);
+    secondsEl.textContent = addLeadingZero(seconds);
+  }, 1000);
+}
+startButton.addEventListener('click', startCountdown);
+flatpickr('#datetime-picker', options);
